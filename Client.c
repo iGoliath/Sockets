8770771c730d059caf5445cf6bbed2b2300b3516
iGoliath <mdajs60@gmail.com>
2025-02-16 22:26:52 -0500

    Program finished up to the point of which it takes 3 clients, and properly validates the given username/password up to 3 attempts. Will try to add password masking.

--- Client.c
+++ Client.c
@@ -35,16 +35,26 @@ int main(int argc, char *argv[]) {
     serv_addr.sin_port = htons(portno);
     if (connect(sockfd,(struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
     error("ERROR connecting");
-    printf("Welcome to the Computer Science Department\n\nLogin: ");
+    for (int i = 0; i < 3; i++) { 
+    printf("\n\nWelcome to the Computer Science Department\n\nLogin: ");
     bzero(buffer, 256);
     fgets(buffer,255,stdin);
     n = write(sockfd, buffer, strlen(buffer));
     if (n < 0)
     error("ERROR writing to socket");
     bzero(buffer, 256);
+    printf("Password: ");
+    fgets(buffer, 255, stdin);
+    n = write(sockfd, buffer, strlen(buffer));
+    bzero(buffer, 256);
     n = read(sockfd, buffer, 255);
-    if (n < 0)
-    error("ERROR reading from socket");
-    printf("%s\n", buffer);
+    if (n < 0) error("ERROR reading from socket");
+    printf("%s", buffer);
+    if (buffer[0] == 'W') {
+        break;
+    }
+    }
+
+    
     return 0;
 }
--- Server.c
+++ Server.c
@@ -4,30 +4,81 @@
 #include <netinet/in.h>
 #include <netdb.h>
 #include <string.h>
+#include <pthread.h>
 
 void error(char *msg) {
     perror(msg);
     exit(1);
 }
 
-void createClientInstance(int socketfd) {
-   
-    char buffer[256];
-    printf("");
-    int n = read(socketfd, buffer, 255);
-    if (n < 0) error("ERROR reading from socket");
-    printf("%s", buffer);
+char fileEntries[25][20];
+const char *accessMessage = "Welcome to CPSC445-Comp Networking Class\nYou are invited to use %s's Machine!";
 
+void* createClientInstance(void* args) {
+   
+    int *socketfd = (int *)args;
+    char userNamebuffer[256];
+    char passwdbuffer[256];
+    char msgbuffer[256];
+    int n;
+    
+    for(int i = 0; i < 3; i++) {
+    bzero(userNamebuffer, 256);
+    bzero(passwdbuffer, 256);
+    printf("");
+   
+    n = read(*socketfd, userNamebuffer, 255);
+    if (n < 0) error("ERROR reading from socket");
+    n = read(*socketfd, passwdbuffer, 255);
+    if (n < 0) error("ERROR reading from socket");
+
+    if ((memcmp(userNamebuffer, fileEntries[0], 5) == 0) && (memcmp(passwdbuffer, fileEntries[1], 4) == 0)) {
+        snprintf(msgbuffer, sizeof(msgbuffer), accessMessage, fileEntries[2]);
+        n = write(*socketfd, msgbuffer, 255);
+        return NULL;
+    }
+    else if ((memcmp(userNamebuffer, fileEntries[5], 5) == 0) && (memcmp(passwdbuffer, fileEntries[6], 4) == 0)) {
+        snprintf(msgbuffer, sizeof(msgbuffer), accessMessage, fileEntries[7]);
+        n = write(*socketfd, msgbuffer, 255);
+        return NULL;
+    }
+    else if ((memcmp(userNamebuffer, fileEntries[10], 5) == 0) && (memcmp(passwdbuffer, fileEntries[11], 4) == 0)) {
+        snprintf(msgbuffer, sizeof(msgbuffer), accessMessage, fileEntries[12]);
+        n = write(*socketfd, msgbuffer, 255);
+        return NULL;
+    }
+    else if ((memcmp(userNamebuffer, fileEntries[15], 5) == 0) && (memcmp(passwdbuffer, fileEntries[16], 4) == 0)) {
+        snprintf(msgbuffer, sizeof(msgbuffer), accessMessage, fileEntries[17]);
+        n = write(*socketfd, msgbuffer, 255);
+        return NULL;
+    }
+    else if ((memcmp(userNamebuffer, fileEntries[20], 5) == 0) && (memcmp(passwdbuffer, fileEntries[21], 4) == 0)) {
+        snprintf(msgbuffer, sizeof(msgbuffer), accessMessage, fileEntries[22]);
+        n = write(*socketfd, msgbuffer, 255);
+        return NULL;
+    }
+    else if (i < 2){
+    n = write(*socketfd, "You have entered your username or password incorrectly\nPlease try again", 255);
+    }
+    else {
+    n = write(*socketfd, "You have exceeded the maximum number of allowed attemps to login.\nOnly for ESU CPSC Students taking CPSC445.\nYou are not yet invited.\n", 255);
+    return NULL;
+    }
+
+    }
+   return NULL;
 }
+
+ 
 
 int main(int argc, char *argv[]) {
     FILE *file_ptr = fopen("passwd.txt", "r");
-    char fileEntries[25][20];
     char ch;
     int index = 0;
     for (int i = 0; i < 25; i++) {
         index = 0;
         while ((ch = fgetc(file_ptr)) != EOF) {
+            if (ch == '\n') continue;
             if (ch == ';')
             break;
             fileEntries[i][index] = ch;
@@ -58,17 +109,22 @@ int main(int argc, char *argv[]) {
     listen(sockfd, 5);
     clilen = sizeof(cli_addr);
 
-    int fds[3];
-
-    for (int i = 0; i < 3; i++) {
+   
+   pthread_t threads[3];
+   
+    for(int i = 0; i < 3; i++) {
         newsockfd = accept(sockfd, (struct sockaddr*) & cli_addr, &clilen);
         if (newsockfd < 0)
         error("ERROR on accept");
-        fds[i] = newsockfd;
+        int* newsockfd_ptr = malloc(sizeof(int));
+        *newsockfd_ptr = newsockfd;
+        pthread_create(&threads[i], NULL, createClientInstance, newsockfd_ptr);
     }
 
-    createClientInstance(fds[0]);
-    createClientInstance(fds[1]);
-    createClientInstance(fds[2]);
+    for (int i = 0; i < 3; i++) {
+        pthread_join(threads[i], NULL);
+    }
 
+    close(sockfd);
+    return 0;
 }
